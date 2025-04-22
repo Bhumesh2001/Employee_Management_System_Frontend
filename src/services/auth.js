07bd2946aaf646dcbb371a9d86de6006fcdfa89f
bhumesh2001 <bhumesh21@navgurukul.org>
2025-04-22 14:37:25 +0530

    new code

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,16 +2,18 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true; // send cookies with every request
 
+let BackendUrl = 'https://employee-management-system-backend-taupe.vercel.app';
+
 export const login = async (email, password, role) => {
-    const response = await axios.post('/api/auth/login', { email, password, role });
+    const response = await axios.post(`${BackendUrl}/api/auth/login`, { email, password, role });
     return response.data.data;
 };
 
 export const getUserProfile = async () => {
-    const response = await axios.get('/api/auth/profile');
+    const response = await axios.get(`${BackendUrl}/api/auth/profile`);
     return response.data.data;
 };
 
 export const logout = async (role) => {
-    await axios.post('/api/auth/logout', { role });
+    await axios.post(`${BackendUrl}/api/auth/logout`, { role });
 };
